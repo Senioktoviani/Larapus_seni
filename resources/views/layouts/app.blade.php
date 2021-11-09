4e193e009ec05e509bbb5957b1955174b889eae5
Senioktoviani <senioktoviani148@gmail.com>
2021-11-09 11:30:09 +0700

    Laratrust

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,12 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="fixed-bottom">
+            <center>
+                Copyright &copy; Larapus Kayla Rahmanisa - {{date('Y')}}
+            </center>
+        </footer>
     </div>
 </body>
 </html>
